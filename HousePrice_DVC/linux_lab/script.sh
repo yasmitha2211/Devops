b2ce82b92236720b706d6e9a4f1cf4b757d7259a
yasmitha2211 <yasmitha2211@gmail.com>
2026-08-06 01:46:45 +0530

    Added Lab-02 HousePrice_DVC project and removed linux_lab

--- HousePrice_DVC/linux_lab/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Welcome to Linux Lab"
-echo "Current User: $USER"
-echo "Current Working Directory: $(pwd)"
-echo "Current Date and Time: $(date)"
-echo "Course Name: $COURSE_NAME"
